7092e92ee5344539d1547cfee9b7a8cf15a2b445
Aleksandr Panchul <apanchul@hotmail.com>
2020-05-20 19:54:47 -0700

    adding default yes

--- gpu_enabled/update_docker_ce.sh
+++ gpu_enabled/update_docker_ce.sh
@@ -5,11 +5,11 @@
 # automating what is in https://docs.docker.com/engine/install/ubuntu/
 #
 
-sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
 # ...
 sudo apt-get update
 # ...
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 # ...
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 #ok
@@ -23,7 +23,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 # ...
 sudo apt-get update
 # ...
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 # ...
 # This is for installing a specific version
 # apt-cache madison docker-ce
